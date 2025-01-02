3543757cc38ac199223566720f7e524ec19fd786
Mateusz Pawelczyk <mateuszpawelczyk2003@gmail.com>
2025-01-02 21:07:26 +0100

    Integrate Polyscope, Eigen, and tinyobjloader with CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <Eigen/Core>
+#include <polyscope/polyscope.h>
 
 int main() {
-    std::cout << "Hello, Graphics World!" << std::endl;
+    std::cout << "Initializing Polyscope..." << std::endl;
+
+    polyscope::init();
+    polyscope::show();
+
+    Eigen::Vector3f vec(1.0f, 2.0f, 3.0f);
+    std::cout << "Eigen Vector: " << vec.transpose() << std::endl;
+
     return 0;
 }
