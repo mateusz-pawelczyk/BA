367e2e0ef8a0e54c3f580fa03cac52f2b855f76e
Mateusz Pawelczyk <mateuszpawelczyk2003@gmail.com>
2025-01-02 20:37:11 +0100

    Initial project structure and build setup

---
+++ src/graphics/Graphics.cpp
@@ -0,0 +1,6 @@
+#include "graphics/Graphics.h"
+#include <iostream>
+
+void render() {
+    std::cout << "Rendering a scene..." << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Graphics World!" << std::endl;
+    return 0;
+}
